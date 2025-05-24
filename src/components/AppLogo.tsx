1cea9ba29eafdf8d1ba2ad10911fec639801ec00
jiangyin14 <1219439938@qq.com>
2025-05-24 16:37:14 +0800

    fix: Update AppLogo component to use a different image and adjust dimensions for better display

--- src/components/AppLogo.tsx
+++ src/components/AppLogo.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Image from 'next/image'; // Import Next.js Image component
-import clotherIcon from '@/assets/clother_icon.png'; // Import the image
+import clotherIcon from '@/assets/banner.png'; // Import the image
 
 const AppLogo = () => {
   return (
     <div className="flex items-center space-x-2">
       {/* Replace text with the Image component */}
-      <Image src={clotherIcon} alt="Clother Icon" width={32} height={32} />
+      <Image src={clotherIcon} alt="Clother Icon" width={240} height={32} />
     </div>
   );
 };
